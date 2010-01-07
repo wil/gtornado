89f2df13d003c63d32f8ca669a565e5cefb69a81
Wil Tan <wil@dready.org>
2010-01-08 00:16:50 +1100

    example programs

---
+++ src/tsockhttp.py
@@ -0,0 +1,35 @@
+# taken from http://nichol.as/asynchronous-servers-in-python
+
+import errno
+import functools
+import socket
+from tornado import ioloop, iostream
+
+
+def connection_ready(sock, fd, events):
+    while True:
+        try:
+            connection, address = sock.accept()
+        except socket.error, e:
+            if e[0] not in (errno.EWOULDBLOCK, errno.EAGAIN):
+                raise
+            return
+        connection.setblocking(0)
+        stream = iostream.IOStream(connection)
+        stream.write("HTTP/1.0 200 OK\r\nContent-Length: 5\r\n\r\nPong!\r\n", stream.close)
+
+if __name__ == '__main__':
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    sock.setblocking(0)
+    sock.bind(("", 8010))
+    sock.listen(5000)
+
+    io_loop = ioloop.IOLoop.instance()
+    callback = functools.partial(connection_ready, sock)
+    io_loop.add_handler(sock.fileno(), callback, io_loop.READ)
+    try:
+        io_loop.start()
+    except KeyboardInterrupt:
+        io_loop.stop()
+        print "exited cleanly"
